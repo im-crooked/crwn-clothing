046c5de0cdbfda02dae1ed080ddd84aa713567aa
im-crooked <mundesanjay04@gmail.com>
2021-05-07 04:16:19 +0530

    added components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+
+import './homepage.styles.scss'
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory></Directory>
+    </div>
+  );
+};
+
+export default HomePage;
